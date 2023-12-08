0b22741ca67d5e70180b336de43854f4745bd466
kimAeris <142148169+kimAeris@users.noreply.github.com>
2023-12-08 12:48:08 +0900

    Create update_readme.py

---
+++ update_readme.py
@@ -0,0 +1,24 @@
+import feedparser
+
+somjang_blog_rss_url = "https://ohzlsss.tistory.com/rss"
+rss_feed = feedparser.parse(somjang_blog_rss_url)
+
+MAX_POST_NUM = 5
+
+latest_blog_post_list = ""
+
+MAX_POST_NUM = 5
+
+for idx, feed in enumerate(rss_feed['entries']):
+    if idx > MAX_POST_NUM:
+        break
+    feed_date = feed['published_parsed']
+    latest_blog_post_list += f"[{feed_date.tm_year}/{feed_date.tm_mon}/{feed_date.tm_mday} - {feed['title']}]({feed['link']}) <br>\n"
+    
+markdown_text = """ ### Hi there 👋
+"""
+
+readme_text = f"{markdown_text}{latest_blog_post_list}"
+
+with open("README.md", 'w', encoding='utf-8') as f:
+    f.write(readme_text)
